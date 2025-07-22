27a571b5c05bf331500eadc2942f8b418ba6c49f
O.G. Brown, III <ogbrown3@gmail.com>
2025-07-22 08:20:31 -0400

    add private subnet support

--- modules/net/main.tf
+++ modules/net/main.tf
@@ -71,3 +71,51 @@ resource "aws_subnet" "use2-a-public" {
     }
   )
 }
+
+resource "aws_subnet" "use2-c-private" {
+  vpc_id                   = aws_vpc.vpc-use2-public.id
+  availability_zone_id     = var.availability_zone_ids[2]
+  cidr_block               = var.cidr_blocks["private_subnet_c"]
+  map_public_ip_on_launch  = false
+  ipv6_native              = false
+  tags = merge(
+    local.net_tags,
+    {
+      Name = "${var.short_project_name}-sub-e2-c-priv"
+    }, {
+      "Public" = "false"
+    }
+  )
+}
+
+resource "aws_subnet" "use2-b-private" {
+  vpc_id                   = aws_vpc.vpc-use2-public.id
+  availability_zone_id     = var.availability_zone_ids[1]
+  cidr_block               = var.cidr_blocks["private_subnet_b"]
+  map_public_ip_on_launch  = false
+  ipv6_native              = false
+  tags = merge(
+    local.net_tags,
+    {
+      Name = "${var.short_project_name}-sub-e2-b-priv"
+    }, {
+      "Public" = "false"
+    }
+  )
+}
+
+resource "aws_subnet" "use2-a-private" {
+  vpc_id                   = aws_vpc.vpc-use2-public.id
+  availability_zone_id     = var.availability_zone_ids[0]
+  cidr_block               = var.cidr_blocks["private_subnet_a"]
+  map_public_ip_on_launch  = false
+  ipv6_native              = false
+  tags = merge(
+    local.net_tags,
+    {
+      Name = "${var.short_project_name}-sub-e2-a-priv"
+    }, {
+      "Public" = "false"
+    }
+  )
+}
